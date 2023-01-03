6a7f96dd391c80983c5544ed28a31fc342d9b82f
clement-crippa <clement.crippa@laplateforme.io>
2023-01-03 17:52:21 +0100

    Fix du triangle impossible
    Il était devenue impossible d avoir l option: Il n'est pas possible de construire un triangle

--- jour02/jobBonus/JobBonus.py
+++ jour02/jobBonus/JobBonus.py
@@ -1,10 +1,10 @@
 def triangle(ab, ac, bc):
-    if (ab + ac > bc) or (ab + bc > ac) or (bc + ac > ab):
+    if (ab + ac > bc) and (ab + bc > ac) and (bc + ac > ab):
         print("Il est possible de construire un triangle")
         if (bc * bc == (ac * ac) + (ab * ab)) and (ab == ac or ab == bc or ac == bc) or (
                 ab * ab == (ac * ac) + (bc * bc)) and (ab == ac or ab == bc or ac == bc) or (
                 ac * ac == (bc * bc) + (ab * ab)) and (ab == ac or ab == bc or ac == bc):
-            print("C'est un triangle regtangle isocèle")
+            print("C'est un triangle rectangle isocèle")
         elif (bc * bc == (ac * ac) + (ab * ab)) or (ab * ab == (ac * ac) + (bc * bc)) or (
                 ac * ac == (bc * bc) + (ab * ab)):
             print("C'est un triangle rectangle")
@@ -22,3 +22,4 @@ triangle(8, 8, 8)
 triangle(8, 8, 10)
 triangle(float(5.5), float(7.3), float(4.8))
 triangle(float(3), float(5.5), float(7.5))
+triangle(1, 2, 10)
